8e17280c8d38fe7994acd9218a992ef462cdde3e
Fratila Aida <fratila.aida16@yahoo.com>
2023-05-17 03:43:17 -0400

    C4

--- main.c
+++ main.c
@@ -43,7 +43,8 @@ void addAtBeginning(Team** head, Team* newTeam) {
     *head = newTeam;
 }
 Team * findMin(Team *team_head){ //Functia pentru a gasi cel mai mic nod din lista
-Team* minNode=team_head;
+Team* minNode=(Team*)malloc(sizeof(Team));
+minNode=team_head;
 while(team_head!=NULL){
     if(team_head->ma < minNode->ma) minNode=team_head;
     team_head=team_head->next;
@@ -63,11 +64,11 @@ int main(int argc, char *argv[]){
         printf("Eroare la deschiderea fisierului");
         exit(1);
     }
-    int x=0;
+    int x[5];
 Team *team_head=NULL; 
-for(int i=1;i<5;i++)
-{fscanf(f_cerinte,"%d",&x);
-if(x==1) if(i==1)
+for(int i=1;i<=5;i++)
+fscanf(f_cerinte,"%d",&x[i]);
+ if(x[1]==1 && x[2]==0)
 {
     f_read = fopen(argv[2],"r");
     if(f_read==NULL){
@@ -105,40 +106,75 @@ if(x==1) if(i==1)
         addAtBeginning(&team_head, newTeam);
     }
 
+Team *team_head_aux=(Team*)malloc(sizeof(Team)); 
+team_head_aux=team_head;
 //fprintf(r_out,"%d\n",nr_teams);
-while(team_head!=NULL){
+while(team_head_aux!=NULL){
     //fprintf(r_out,"%d ",team_head->nr_players);
-    fprintf(r_out,"%s",team_head->teamName);
+    fprintf(r_out,"%s",team_head_aux->teamName);
    // for(int i=0;i<team_head->nr_players;i++){
    //             fprintf(r_out,"%s ",team_head->players[i].firstName);
     //            fprintf(r_out,"%s ",team_head->players[i].secondName);
     //            fprintf(r_out,"%d\n",team_head->players[i].points);
    // }
-    team_head=team_head->next;
+    team_head_aux=team_head_aux->next;
 }
+//fprintf(r_out,"/n");
 }
-else if(i==2)
+else if(x[1]==1 &&x[2]==1)
 { 
 int p=1;
 while(p<nr_teams) p*=2;
 p=p/2;
-fprintf(r_out,"%d",p);
+//fprintf(r_out,"%d",p);
 int count=0;//cate eliminari fac
+Team *aux=(Team*)malloc(sizeof(Team));
 
-while(team_head!=NULL){
+aux=team_head;
+float x=0;
+while(aux!=NULL){ 
     int sum=0;
-    for(int i=0;i<team_head->nr_players;i++){
-         sum+=team_head->players[i].points;
-    }
-    team_head->ma=sum/(team_head->nr_players);
+    for(int i=0;i<aux->nr_players;i++)
+         {sum=sum+aux->players[i].points;
+           //fprintf(r_out,"%d ",aux->players[i].points);
+         }
+        //fprintf(r_out,"\n");
+   // fprintf(r_out,"%d\n",sum);
+    x=((1.0*sum)/(aux->nr_players));
+    aux->ma=x;
+   // fprintf(r_out,"%.2f\n",x);
+    aux=aux->next;
+}
+
+   
+   Team* prev=(Team*)malloc(sizeof(Team));
+while(count<nr_teams-p){ 
+Team* current=(Team*)malloc(sizeof(Team));
+Team* minNode=findMin(team_head);
+
+  current=team_head;
+  prev=NULL;
+  while(current!=NULL && current->ma!=minNode->ma){
+    prev=current;
+    current=current->next;}
+
+  if(prev==NULL)  team_head=current->next;
+  
+        else
+                 prev->next=current->next;
+
+    free(current);
+
+    count++;}
+
+//void afisareLista(team_head); 
+while(team_head!=NULL){
+fprintf(r_out,"%s",team_head->teamName);
     team_head=team_head->next;
 }
-while(count<nr_teams-p){ //cate eliminari fac
- 
-    count++;
-}
+//fprintf(r_out,"%d",count); 
+} 
 
-}}
 fclose(r_out);
     fclose(f_read);
     return 0;
